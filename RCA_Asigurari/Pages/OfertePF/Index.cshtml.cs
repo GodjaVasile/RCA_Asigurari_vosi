2522eb3d73595a69f07824f180bf10bb4e2428ed
Godja Vasile <vasile.godja8@gmail.com>
2023-04-12 22:04:40 +0300

    cel mai de pret proiect

--- RCA_Asigurari/Pages/OfertePF/Index.cshtml.cs
+++ RCA_Asigurari/Pages/OfertePF/Index.cshtml.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using RCA_Asigurari.Data;
 using RCA_Asigurari.Migrations;
 using RCA_Asigurari.Models;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
 
 namespace RCA_Asigurari.Pages.OfertePF
 {
@@ -23,8 +27,14 @@ namespace RCA_Asigurari.Pages.OfertePF
 
         public IList<OfertaPF> OfertaPF { get; set; } = default!;
         public String CurrentFilter { get; set; }
+
+
+      
+
         public async Task OnGetAsync(string searchString)
         {
+           
+
             CurrentFilter = searchString;
 
             if (_context.OfertaPF != null)
@@ -40,7 +50,7 @@ namespace RCA_Asigurari.Pages.OfertePF
 
                 OfertaPF = await ofertePF.ToListAsync();
 
-
+               
             }
             //if (!String.IsNullOrEmpty(searchString))
             //{
@@ -61,7 +71,73 @@ namespace RCA_Asigurari.Pages.OfertePF
             //}
 
         }
-     
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // Obțineți lista de obiecte OfertaPF din baza de date
+            var ofertePF = await _context.OfertaPF
+                .Include(o => o.CategorieVehicul)
+                .Include(o => o.Client)
+                .Include(o => o.TipCombustibil)
+                .ToListAsync();
+
+            // Pentru fiecare obiect din lista, generați un fișier PDF separat
+            foreach (var oferta in ofertePF)
+            {
+                // Generați PDF-ul folosind metoda GeneratePdfFromObjectData() pentru obiectul curent
+                GeneratePdfFromObjectData(oferta);
+
+                // Așteptați un moment pentru a evita suprasolicitarea serverului
+                await Task.Delay(1000);
+            }
+
+            // Returnați un redirect către pagina curentă
+            return RedirectToPage();
+        }
+
+        public void GeneratePdfFromObjectData(OfertaPF oferta)
+        {
+            // Creați un nou document MigraDoc
+            Document document = new Document();
+
+            // Adăugați o nouă secțiune la document
+            Section section = document.AddSection();
+
+            // Adăugați o nouă tabelă la secțiune
+            MigraDoc.DocumentObjectModel.Tables.Table table = section.AddTable();
+
+            // Adăugați coloane la tabelă
+            table.AddColumn(Unit.FromCentimeter(2));
+            table.AddColumn(Unit.FromCentimeter(5));
+            table.AddColumn(Unit.FromCentimeter(3));
+
+            // Adăugați antete la tabelă
+            Row headerRow = table.AddRow();
+            headerRow.Cells[0].AddParagraph("ID");
+            headerRow.Cells[1].AddParagraph("Pret");
+            headerRow.Cells[2].AddParagraph("Masa");
+
+            // Adăugați rânduri de date la tabel
+            Row dataRow = table.AddRow();
+            dataRow.Cells[0].AddParagraph(oferta.CNP.ToString());
+            dataRow.Cells[1].AddParagraph(oferta.SerieCI.ToString());
+            dataRow.Cells[2].AddParagraph(oferta.Pret.ToString());
+
+            // Creați un nou procesor de documente PDF
+            PdfDocumentRenderer renderer = new PdfDocumentRenderer();
+
+            // Setează documentul MigraDoc pentru a fi randat
+            renderer.Document = document;
+
+            // Randează documentul într-un fișier PDF
+            renderer.RenderDocument();
+            string fileName = "Oferta_" + DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + oferta.ID.ToString() + ".pdf";
+            renderer.Save(fileName);
+        }
+
+
+
+
+
     }
 }
 
